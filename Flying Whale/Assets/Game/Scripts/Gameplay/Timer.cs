ebb4c7e4cbc1dd282359924452eabc6016babd88
MuffinMuffinski <magda.schmitz@yahoo.com>
2021-08-14 00:44:14 +0200

    Countdown displayed in minutes and seconds

---
+++ Flying Whale/Assets/Game/Scripts/Gameplay/Timer.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField]
+    private float m_timeValue;
+
+    public TextMeshProUGUI timerText;
+
+    void Update()
+    {
+        if (m_timeValue > 0)
+        {
+            m_timeValue -= Time.deltaTime;
+        }
+        else m_timeValue = 0;
+
+        DisplayTime(m_timeValue);
+    }
+
+    private void DisplayTime(float timeToDisplay)
+    {
+        if(timeToDisplay < 0)
+        {
+            timeToDisplay = 0;
+        }
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        
+    }
+}

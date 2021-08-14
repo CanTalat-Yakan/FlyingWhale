5b8e24f6f53b654b6b3c1ad54fe3bee8b56e8357
MuffinMuffinski <magda.schmitz@yahoo.com>
2021-08-14 15:16:31 +0200

    own spawnpoints() method

--- Flying Whale/Assets/Game/Scripts/Gameplay/ItemSpawner.cs
+++ Flying Whale/Assets/Game/Scripts/Gameplay/ItemSpawner.cs
@@ -21,38 +21,43 @@ public class ItemSpawner : MonoBehaviour
         //check if enough spawnpoints for all items in total
         if (m_itemCount * m_itemPrefabs.Count < m_spawnpoints.Count)
         {
-            //Loop for every item type
-            for (int item = 0; item < m_itemPrefabs.Count; item++)
-            {
-                //Loop all items of one type
-                for (int i = 0; i < m_itemCount; i++)
-                {
-                    //spawnpoint can only be used once
-                    bool freeSpawnPoint = false;
+            SpawnItems();
+        }
+        else Debug.Log("Not enough spawnpoints available for number of items");
+    }
 
-                    while (!freeSpawnPoint)
+    void SpawnItems()
+    {
+        //Loop for every item type
+        for (int item = 0; item < m_itemPrefabs.Count; item++)
+        {
+            //Loop all items of one type
+            for (int i = 0; i < m_itemCount; i++)
+            {
+                //spawnpoint can only be used once
+                bool freeSpawnPoint = false;
+
+                while (!freeSpawnPoint)
+                {
+                    int rnd = Random.Range(0, m_spawnpoints.Count);
+                    if (m_spawnpoints[rnd] != null)
                     {
-                        int rnd = Random.Range(0, m_spawnpoints.Count);
-                        if (m_spawnpoints[rnd] != null)
-                        {
-                            //Instantiate object of current item type at random generated spawnpoint
-                            Instantiate(m_itemPrefabs[item], m_spawnpoints[rnd].transform.position, Quaternion.identity);
-                            m_spawnpoints[rnd] = null;
-                            freeSpawnPoint = true;
-                        }
+                        //Instantiate object of current item type at random generated spawnpoint
+                        Instantiate(m_itemPrefabs[item], m_spawnpoints[rnd].transform.position, Quaternion.identity);
+                        m_spawnpoints[rnd] = null;
+                        freeSpawnPoint = true;
                     }
                 }
             }
-            //Spawning special item in first available spot
-            for(int i = 0; i < m_spawnpoints.Count; i++)
+        }
+        //Spawning special item in first available spot
+        for (int i = 0; i < m_spawnpoints.Count; i++)
+        {
+            if (m_spawnpoints[i] != null)
             {
-                if(m_spawnpoints[i] != null)
-                {
-                    Instantiate(m_specialItemPrefab, m_spawnpoints[i].transform.position, Quaternion.identity);
-                    return;
-                }
+                Instantiate(m_specialItemPrefab, m_spawnpoints[i].transform.position, Quaternion.identity);
+                return;
             }
         }
-        else Debug.Log("Not enough spawnpoints available for number of items");
     }
 }
